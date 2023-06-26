54e821c8122551b196b1ee98b9a879728b7287c2
Bryan Joseph <bj97301@gmail.com>
2023-06-26 09:09:46 -0700

    add add skia and reanimated

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import { useEffect } from "react";
+import { Canvas, Circle, Group } from "@shopify/react-native-skia";
+import {
+  useDerivedValue,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from "react-native-reanimated";
+export const HelloWorld = () => {
+  const size = 256;
+  const r = useSharedValue(0);
+  const c = useDerivedValue(() => size - r.value);
+  useEffect(() => {
+    r.value = withRepeat(withTiming(size * 0.33, { duration: 1000 }), -1);
+  }, [r, size]);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Canvas style={{ flex: 1 }}>
+      <Group blendMode="multiply">
+        <Circle cx={r} cy={r} r={r} color="cyan" />
+        <Circle cx={c} cy={r} r={r} color="magenta" />
+        <Circle cx={size / 2} cy={c} r={r} color="yellow" />
+      </Group>
+    </Canvas>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default HelloWorld;
---
+++ readme.md
@@ -0,0 +1,39 @@
+# steps
+
+`npx create-expo-app skiaReanimatedBug`
+
+`yarn`
+
+removed package.lock
+
+`npx expo install @shopify/react-native-skia`
+
+<image src="./assets/skia.png">
+
+`npx expo install react-native-reanimated`
+
+<image src="./assets/reanimated.png">
+
+Note that reanimated installed v2.14.4
+
+removed v2 from package.json
+
+removed v2 from yarn lock
+
+removed node_modules folder
+
+`yarn`
+
+`yarn add react-native-reanimated@3.3.0`
+
+<image src="./assets/reanimated 3.png">
+
+
+copy and paste the circles example then add `export default` 
+https://shopify.github.io/react-native-skia/docs/animations/reanimated/#hello-world
+
+
+
+`yarn start`
+
+press i for simulator
